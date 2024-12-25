1be3493aae6e2f3b02eee5843a8ea615c8fc2246
BazZziliuS <bazzzil123@gmail.com>
2024-12-25 08:18:55 +0300

    Обновлена информация

--- scripts/addon.js
+++ scripts/addon.js
@@ -1160,22 +1160,22 @@
                 }, 100);
             }
         });
-        Lampa.SettingsApi.addParam({
-            component: 'add_management_plugin',
-            param: {
-                name: 'Reboot_management_plugin',
-                type: 'static',
-            },
-            field: {
-                name: '<div class="settings-folder" style="padding:0!important"><div style="display: block; margin: 0 auto;height:2.3em;padding-right:.1em"><svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g style="fill:none;stroke:#ffffff;stroke-width:12px;stroke-linecap:round;stroke-linejoin:round;"> <path d="m 50,10 0,35"></path> <path d="M 20,29 C 4,52 15,90 50,90 85,90 100,47 74,20"></path> </g> <path style="fill:#ffffff;" d="m 2,21 29,-2 2,29"></path> </g></svg></div></div>',
-                description: '<div style="text-align: center;">Нажмите для перезагрузки Lampa</div>',
-            },
-            onRender: function (item) {
-                item.on('hover:enter', function () {
-                    location.reload();
-                });
-            }
-        });
+        // Lampa.SettingsApi.addParam({
+        //     component: 'add_management_plugin',
+        //     param: {
+        //         name: 'Reboot_management_plugin',
+        //         type: 'static',
+        //     },
+        //     field: {
+        //         name: '<div class="settings-folder" style="padding:0!important"><div style="display: block; margin: 0 auto;height:2.3em;padding-right:.1em"><svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g style="fill:none;stroke:#ffffff;stroke-width:12px;stroke-linecap:round;stroke-linejoin:round;"> <path d="m 50,10 0,35"></path> <path d="M 20,29 C 4,52 15,90 50,90 85,90 100,47 74,20"></path> </g> <path style="fill:#ffffff;" d="m 2,21 29,-2 2,29"></path> </g></svg></div></div>',
+        //         description: '<div style="text-align: center;">Нажмите для перезагрузки Lampa</div>',
+        //     },
+        //     onRender: function (item) {
+        //         item.on('hover:enter', function () {
+        //             location.reload();
+        //         });
+        //     }
+        // });
 
         Lampa.Settings.listener.follow('open', function (e) {
             if (e.name == 'main') {
