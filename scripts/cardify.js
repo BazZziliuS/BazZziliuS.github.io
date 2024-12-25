1258199426b04d63fcfbea9f89ce2cbdab7ed32c
BazZziliuS <leymankostya@mail.ru>
2024-12-25 07:47:55 +0300

    Update cardify.js

--- scripts/cardify.js
+++ scripts/cardify.js
@@ -850,9 +850,6 @@
     if (!Lampa.Platform.screen("tv")) {
       return console.log("Cardify", "no tv");
     }
-    if (!Lampa.Account.hasPremium()) {
-      return console.log("Cardify", "no premium");
-    }
     Lampa.Lang.add({
       cardify_enable_sound: {
         ru: "Включить звук",
