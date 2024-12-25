c944b4c21457379ddd6cc81699f2d06660da8f5b
BazZziliuS <bazzzil123@gmail.com>
2024-12-25 16:35:31 +0300

    modified:   scripts/load.js

--- scripts/load.js
+++ scripts/load.js
@@ -1,59 +1,60 @@
 (function () {
-  "use strict";
-  window.Lampa.Plugins.load = new Proxy(window.Lampa.Plugins.load, {
-    apply(a, b, c) {
-      console.log("!Interceptor: Intercepted call to window.Lampa.Plugins.load");
-      console.log("!Plugins", "start load");
-      modify();
-      loadBlackList(d => {
-        Account.plugins(e => {
-          let f = window.lampa_settings.plugins_use ? e.filter(a => a.status).map(a => a.url).concat(Storage.get("plugins", "[]").filter(a => a.status).map(a => a.url)) : [];
-          f.push("./plugins/modification.js");
-          f = f.filter((a, b) => {
-            return f.indexOf(a) === b;
-          });
-          console.log("!Plugins", "load list:", f);
-          b._blacklist = d;
-          d.forEach(a => {
-            f = f.filter(b => b.toLowerCase().indexOf(a) == -1);
-          });
-          console.log("!Plugins", "clear list:", f);
-          let g = [];
-          let h = {};
-          let i = [];
-          f.forEach(a => {
-            let b = addPluginParams(a);
-            i.push(b);
-            h[b] = a;
-          });
-          Utils.putScriptAsync(i, () => {
-            Reflect.apply(a, b, c);
-            delete b._blacklist;
-            delete b.analysisPlugins;
-          }, a => {
-            if (a.indexOf("modification.js") == -1) {
-              console.log("Plugins", "error:", h[a]);
-              g.push(h[a]);
-              createPluginDB(h[a], a);
-            }
-          }, a => {
-            console.log("!Plugins", "include:", h[a]);
-            b._created.push(h[a]);
-            updatePluginDB(h[a], a);
-            delete b.analysisPlugins;
-          }, false);
-        });
-      });
-    }
-  });
+    "use strict";
+    window.Lampa.Plugins.load = new Proxy(window.Lampa.Plugins.load, {
+        apply(a, b, c) {
+            console.log("!Interceptor: Intercepted call to window.Lampa.Plugins.load");
+            console.log("!Plugins", "start load");
+            modify();
+            loadBlackList(d => {
+                Account.plugins(e => {
+                    let f = window.lampa_settings.plugins_use ? e.filter(a => a.status).map(a => a.url).concat(Storage.get("plugins", "[]").filter(a => a.status).map(a => a.url)) : [];
+                    f.push("./plugins/modification.js");
+                    f = f.filter((a, b) => {
+                        return f.indexOf(a) === b;
+                    });
+                    console.log("!Plugins", "load list:", f);
+                    b._blacklist = d;
+                    d.forEach(a => {
+                        f = f.filter(b => b.toLowerCase().indexOf(a) == -1);
+                    });
+                    console.log("!Plugins", "clear list:", f);
+                    let g = [];
+                    let h = {};
+                    let i = [];
+                    f.forEach(a => {
+                        let b = addPluginParams(a);
+                        i.push(b);
+                        h[b] = a;
+                    });
+                    Utils.putScriptAsync(i, () => {
+                        Reflect.apply(a, b, c);
+                        delete b._blacklist;
+                        delete b.analysisPlugins;
+                    }, a => {
+                        if (a.indexOf("modification.js") == -1) {
+                            console.log("Plugins", "error:", h[a]);
+                            g.push(h[a]);
+                            createPluginDB(h[a], a);
+                        }
+                    }, a => {
+                        console.log("!Plugins", "include:", h[a]);
+                        b._created.push(h[a]);
+                        updatePluginDB(h[a], a);
+                        delete b.analysisPlugins;
+                    }, false);
+                });
+            });
+            console.log("!Plugins", "end load");
+        }
+    });
 })();
 (function () {
-  let b;
-  try {
-    const a = Function("return (function() {}.constructor(\"return this\")( ));");
-    b = a();
-  } catch (a) {
-    b = window;
-  }
-  b.setInterval(a, 4000);
+    let b;
+    try {
+        const a = Function("return (function() {}.constructor(\"return this\")( ));");
+        b = a();
+    } catch (a) {
+        b = window;
+    }
+    b.setInterval(a, 4000);
 })();
