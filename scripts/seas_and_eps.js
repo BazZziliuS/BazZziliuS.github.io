7a174fcec0a082e7c4f1123d0fa7ce18491b560a
BazZziliuS <leymankostya@mail.ru>
2025-03-28 13:36:40 +0300

    Update seas_and_eps.js

--- scripts/seas_and_eps.js
+++ scripts/seas_and_eps.js
@@ -1,9 +1,9 @@
-;
 (function () {
   'use strict';
 
   Lampa.Platform.tv();
   (function () {
+    "use strict";
     function c() {
       Lampa.SettingsApi.addParam({
         component: "interface",
@@ -18,7 +18,7 @@
         onRender: function (a) {
           // TOLOOK
           setTimeout(function () {
-            $("div[data-name=\"season_and_seria\"]").insertAfter("div[data-name=\"card_interfice_reactions\"]");
+            $("div[data-name=\"season_and_seria\"]").insertAfter("div[data-name=\"card_interfice_cover\"]");
           }, 0);
         }
       });
@@ -43,7 +43,7 @@
                   g = c + " сезон завершен";
                 }
                 if (!$(".card--new_seria", Lampa.Activity.active().activity.render()).length) {
-                  if (window.innerWidth > 585) {
+                  if (window.innerWidth > 585 && !$(".full-start-new.cardify").length) {
                     $(".full-start__poster,.full-start-new__poster", Lampa.Activity.active().activity.render()).append("<div class='card--new_seria' style='right: -0.6em!important;position: absolute;background: #df1616;color: #fff;bottom:.6em!important;padding: 0.4em 0.4em;font-size: 1.2em;-webkit-border-radius: 0.3em;-moz-border-radius: 0.3em;border-radius: 0.3em;'>" + Lampa.Lang.translate(g) + "</div>");
                   } else if ($(".card--new_seria", Lampa.Activity.active().activity.render()).length) {
                     $(".full-start__tags", Lampa.Activity.active().activity.render()).append("<div class=\"full-start__tag card--new_seria\"><img src=\"./img/icons/menu/movie.svg\" /> <div>" + Lampa.Lang.translate(g) + "</div></div>");
