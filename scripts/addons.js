c22dda8dfe63d873a9660d3fda65c15fe6fef71a
BazZziliuS <bazzzil123@gmail.com>
2025-08-29 20:27:26 +0300

    add yandex metrika

--- scripts/addons.js
+++ scripts/addons.js
@@ -262,4 +262,22 @@
             onRender: (item) => renderPluginStatus(item, p.url, p.key),
         });
     });
+
+    /* Счётчик Яндекса */
+    (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) {
+                return;
+            }
+        }
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    ym(87238418, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true })
+    var METRIKA = '<noscript><div><img src="https://mc.yandex.ru/watch/87238418" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
+    $('body').append(METRIKA);
 })();
